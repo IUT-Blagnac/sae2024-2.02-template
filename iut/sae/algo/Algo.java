e8b8d9dbafbd8e032e0ee278aec9aa5c99980c33
Jean-Michel Bruel <jbruel@gmail.com>
2024-06-05 22:29:17 +0200

    first commit

---
+++ iut/sae/algo/Algo.java
@@ -0,0 +1,21 @@
+package iut.sae.algo;
+
+
+public class Algo{
+    public static String RLE(String in){
+        ...
+    }
+
+    public static String RLE(String in, int iteration){
+        ...    
+    }
+
+    public static String unRLE(String in){
+        ...
+    }
+
+    public static String unRLE(String in, int iteration){
+        ...
+    }
+}
+
---
+++ iut/sae/algo/AlgoException.java
@@ -0,0 +1,11 @@
+package iut.sae.algo;
+
+public class AlgoException extends Exception{
+    public AlgoException() {
+        super();
+    }
+
+    public AlgoException(String s) {
+        super(s);
+    }
+}
